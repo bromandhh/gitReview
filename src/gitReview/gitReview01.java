1b6fd65bf933ff43a18f8b23fb1666d5d9497082
Hamid Bromand <bromandhh@gmail.com>
2022-08-29 18:24:30 -0700

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,12 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Today we review learning gitHub.");
+		System.out.println("Today is still same day we are working on learning gitHub.");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author broma
+ *
+ */
+module gitReview {
+}
